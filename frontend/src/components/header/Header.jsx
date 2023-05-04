eb097349c24b4423b2c5ea87699db317f20e011b
Jimmy Vu Tran <38023521+JaayTran@users.noreply.github.com>
2023-05-04 13:56:46 -0400

    added calendar function to choose date for reso

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,7 +1,17 @@
-import { faChair, faCalendarDays } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './header.css';
+import { faChair, faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./header.css";
+import { useState } from "react";
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+import { Calendar } from "primereact/calendar";
+
 const Header = () => {
+  const [date, setDate] = useState(null);
+
   return (
     <div className="header">
       <div className="headerList">
@@ -12,6 +22,15 @@ const Header = () => {
         <div className="headerListItem">
           <FontAwesomeIcon icon={faCalendarDays} />
           <span>Reservation View</span>
+        </div>
+        <div className="p-float-label">
+          <Calendar
+            value={date}
+            onChange={(e) => setDate(e.value)}
+            dateFormat="dd/mm/yy"
+            showButtonBar
+          />
+          <label htmlFor="birth_date">Select Date</label>
         </div>
       </div>
     </div>
